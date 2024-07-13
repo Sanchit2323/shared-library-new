c57e7e489be34bdba09efe99d04464524115a686
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-07-14 01:53:46 +0530

    Update compileJava.groovy

--- vars/compileJava.groovy
+++ vars/compileJava.groovy
@@ -1,5 +1,14 @@
 import org.p9.CompileJavaCode
+import org.p9.SonarqubeJava
 
 def callCompile(script) {
     script.sh 'mvn clean compile'
+}
+
+def call(String projectKey, String sourcesDir, String sonarToken) {
+    new SonarqubeJava().call(projectKey, sourcesDir, sonarToken)
+}
+
+def call(String projectKey, String sourcesDir, String sonarToken, String binariesDir) {
+    new SonarqubeJava().call(projectKey, sourcesDir, sonarToken, binariesDir)
 }
